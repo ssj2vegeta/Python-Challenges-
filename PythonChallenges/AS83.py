833b2c16092c080aae5e8db29013f5402789c844
Haziq Ahmed Rumzy <75434700+ssj2vegeta@users.noreply.github.com>
2022-11-27 17:12:56 +0800

    carpet

---
+++ PythonChallenges/AS83.py
@@ -0,0 +1,27 @@
+import turtle
+tom = turtle.Turtle()
+tom.penup()
+tom.goto(-300,0)
+tom.pendown()
+tom.left(90)
+def polygon(sides,length,repetitions = 4):
+    for i in range(repetitions):
+        for i in range(sides):
+            tom.forward(length)
+            tom.right(360 / sides)
+        if sides % 2 != 0:
+            for i in range(round(sides/2)):
+                tom.forward(length)
+                tom.right(360 / sides)
+            tom.setheading(90)
+            continue
+        if sides % 2 == 0:
+            for i in range(int(sides/2)):
+                tom.forward(length)
+                tom.right(360 / sides)
+            tom.setheading(90)
+            continue
+sides = int(input("how many sides do you want?"))
+length = int(input("how long do you want your sides to be?"))
+polygon(sides, length)
+turtle.done()
